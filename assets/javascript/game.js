626454980402c6d950a2a3af0c160261dfabc316
Rebecca Hom <rebecca.hom001@gmail.com>
2018-10-12 14:48:39 -0700

    added in initial blanks for current word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,7 @@ var game = {
         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     ],
     words: [
-        "eagle",
-        "sparrow",
+        /*"eagle",
         "sperel",
         "superman",
         "comic",
@@ -17,7 +16,10 @@ var game = {
         "lamp",
         "laptop",
         "desktop",
-        "gundam"
+        "gundam",*/
+        "bald eagle",
+        //"worm",
+        "keyboard"
     ],
 
     // Random solution generator
@@ -30,25 +32,47 @@ var game = {
     played: 1,
     attempts: [],
     guesses: 0,
-    word: "",
+    winner: "",
+    current: [],
 
     // Sets up the game
     setup: function() {
         // Resets the solution, guesses, and attempts
-        this.word = this.solution().toLowerCase();
+        this.winner = this.solution().toLowerCase();
         this.guesses = this.totalGuess;
         this.attempts = [];
+        
+        // Creates the blanks for the word
+        this.current = [];
+        var i = 0;
+        
+        while(i < this.winner.length) {
+            // If there's a space, then put a space instead of an underscore
+            if(this.winner[i] == " ") {
+                this.current.push("&nbsp;");
+            } else {
+                this.current.push("_");
+            }
+            i++;
+        }
+
+        // Puts the text on to the page
+        document.getElementById("word").innerHTML = this.current.join(" ");;
+        document.getElementById("guess").innerHTML = this.guesses;
+        document.getElementById("attempt").innerHTML = this.attempts;
     },
 
     // Allows for gameplay
-    play: function() {
+    play: function(guess) {
+        // Checks if the guess is a letter.
+        if(this.letters.indexOf(guess) >= 0) {
 
+        }
     }
 }
 
 // Starts the game
 game.setup();
-
 document.addEventListener("keydown", function(event) {
-
+    game.play(event.key.toLowerCase());
 });
